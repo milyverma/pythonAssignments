b2593923809d77b0f94285903fd52c7b0a7d7b3e
milyverma <milyverma11@gmail.com>
2025-05-15 19:37:57 +0530

    adding a new file

---
+++ task1.py
@@ -0,0 +1,10 @@
+n1 = int(input("Enter the first number"))
+n2 = int(input("Enter the second number"))
+a = n1 + n2
+s = n1 - n2
+m = n1 * n2
+d = n1 / n2
+print("Addition: ", a)
+print("Subtraction: ", s)
+print("Multiplication: ", m)
+print("Division: ", d)
---
+++ task2.py
@@ -0,0 +1,5 @@
+fn = str(input('Enter your first name'))
+sn = str(input('Enter your last name'))
+full_name = fn+' '+sn
+print("Hello, "+full_name+"! Welcome to the Python program.")
+
