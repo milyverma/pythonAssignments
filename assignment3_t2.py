c68b834989b119637eb9ace4abe6b37409968fa6
milyverma <milyverma11@gmail.com>
2025-05-18 00:50:22 +0530

    Assignment 2 tasks

---
+++ assignment3_t2.py
@@ -0,0 +1,14 @@
+from math import *
+
+n = int(input('Enter a number'))
+
+sqrt = sqrt(n)
+log = log(n)
+sin = sin(n)
+print('Square root: ', sqrt)
+print('Logarithm: ', log)
+print('Sine: ', sin)
+
+
+
+
